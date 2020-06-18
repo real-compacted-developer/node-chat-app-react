de62204b5dff4d6f2f33c31c7a404cf61839b694
SkyLightQP <combbm@naver.com>
2020-06-18 12:48:03 +0900

    Feat: input 필드 입력 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Home: React.FC = () => {
+  const history = useHistory();
+
+  const [name, setName] = useState<string>("");
+  const [room, setRoom] = useState<string>("");
+
   return (
     <div className="centered-form">
       <div className="centered-form__form">
@@ -11,14 +17,30 @@ const Home: React.FC = () => {
           </div>
           <div className="form-field">
             <label htmlFor="name">Display name</label>
-            <input type="text" name="name" autoFocus required></input>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              autoFocus
+              required
+            ></input>
           </div>
           <div className="form-field">
             <label htmlFor="room">Room name</label>
-            <input type="text" name="room" required></input>
+            <input
+              type="text"
+              value={room}
+              onChange={(e) => setRoom(e.target.value)}
+              required
+            ></input>
           </div>
           <div className="form-field">
-            <button type="submit">JOIN</button>
+            <button
+              type="submit"
+              onClick={() => history.push(`/chat-room/${room}/${name}`)}
+            >
+              JOIN
+            </button>
           </div>
         </form>
       </div>
